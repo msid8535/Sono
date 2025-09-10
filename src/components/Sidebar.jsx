e5423be69b58f5629d58722ceb9a4aba1939cfb5
msid8535 <mohsinsiddiqui1905@gmail.com>
2025-09-11 01:27:37 +1000

    feat: create playlist button, browse podcasts button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,8 +23,17 @@ const Sidebar = () => {
                 <div className='flex items-center gap-3'>
                     <img className='w-5' src={assets.arrow_icon} alt=''/>
                     <img className='w-5' src={assets.plus_icon} alt=''/>
-                    
                 </div>
+            </div>
+            <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                <h1>Create your first playlist</h1>
+                <p className='font-light'>It's easy, we will help you</p>
+                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 '>Create Playlist</button>
+            </div>
+            <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                <h1>Find some podcasts to follow</h1>
+                <p className='font-light'>We'll keep you updated on the lastest episodes</p>
+                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 '>Browse Podcasts</button>
             </div>
         </div>
     </div>
