09177929017e0fabefa210e12af0922fe334dbfa
msid8535 <mohsinsiddiqui1905@gmail.com>
2025-09-15 04:39:55 +1000

    feat: home page, most played tracks and hottest songs

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -24,6 +24,19 @@ import song3 from './song3.mp3'
 import img1 from './img1.jpg'
 import img2 from './img2.jpg'
 import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './img11.jpg'
+import img12 from './img12.jpg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
 import mic_icon from './mic.png'
 import queue_icon from './queue.png'
 import speaker_icon from './speaker.png'
@@ -32,7 +45,11 @@ import mini_player_icon from './mini-player.png'
 import zoom_icon from './zoom.png'
 import arrow_left from './left_arrow.png'
 import arrow_right from './right_arrow.png'
-
+import bell_icon from './bell.png'
+import like_icon from './like.png'
+import pause_icon from './pause.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
 
 export const assets ={
     arrow_right,
@@ -71,7 +88,7 @@ export const assets ={
 export const songsData =[
     {
         id:0,
-        name: "Wildest Dreams",
+        name: "Is It Over Now?",
         image: img1,
         file: song1,
         desc: "Taylor Swift",
@@ -80,7 +97,7 @@ export const songsData =[
 
     {
         id:1,
-        name: "Rainberry",
+        name: "Entertainer",
         image: img2,
         file: song2,
         desc: "Zayn",
@@ -89,12 +106,97 @@ export const songsData =[
 
     {
         id:2,
-        name: "Biting My Tongue",
+        name: "Golden Years",
         image: img3,
         file: song3,
         desc: "Joshua Bassett",
         duration: "3:10"
     },
+    {
+        id:3,
+        name: "Drivers License",
+        image: img4,
+        file:song1,
+        desc:"Olivia Rodrigo",
+        duration:"2:50"
+    },
+    {
+        id:4,
+        name: "Song Five",
+        image: img5,
+        file:song2,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:10"
+    },
+    {
+        id:5,
+        name: "Song Six",
+        image: img14,
+        file:song3,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:45"
+    },
+    {
+        id:6,
+        name: "Song Seven",
+        image: img7,
+        file:song1,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:18"
+    },
+    {
+        id:7,
+        name: "Song Eight",
+        image: img12,
+        file:song2,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    }
+]
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: img8,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Top 50 India",
+        image: img9,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Trending India",
+        image: img10,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "Trending Global",
+        image: img16,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "Mega Hits",
+        image: img11,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Happy Favorites",
+        image: img15,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
 ]
 
 
