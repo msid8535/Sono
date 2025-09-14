0bbd66b6fd4b40d2dda65f9288ea7417e384af94
msid8535 <mohsinsiddiqui1905@gmail.com>
2025-09-15 02:19:01 +1000

    feat: library options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,6 +15,11 @@ const Navbar = () => {
                 <p className='bg-gray-900 text-white w-7 h-7 rounded-full flex items-center justify-center'>M</p>
             </div>
         </div>
+        <div className='flex items-center gap-2 mt-4'>
+            <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'> All</p>
+            <p className='bg-cyan-800 px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
+            <p className='bg-cyan-800 px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
+        </div>
     </>
   )
 }
